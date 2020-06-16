ce1862c17aafb6a0fdfe07d5def46d353d5d2cdc
shoya1007 <shoya.fujisawa@rakus-partners.co.jp>
2020-06-16 10:49:26 +0900

    新規にプロジェクトを作成

---
+++ src/main/java/com/example/ExBeginner2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBeginner2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExBeginner2Application.class, args);
+	}
+
+}
